5b6b3933aead7e1cbbebebe46f624e49172fa8e6
Sumanthkolla09 <sumanth.kolla009@gmail.com>
2025-01-05 20:55:25 +0530

    To Check the code

--- src/test/java/org/SeleniumTest/EntireE2E.java
+++ src/test/java/org/SeleniumTest/EntireE2E.java
@@ -27,7 +27,7 @@ public class EntireE2E extends BaseTest {
         Assert.assertTrue(match);
         Checkoutpage checkoutpage = cartPage.Checkout();
         checkoutpage.ClickOnCountry("India", "India");
-        System.out.println("Git working check");
+        System.out.println("To test commit singlr file");
 
            /* ConformationPage conformationPage = checkoutpage.SubmtitOrder();
             String conformmessage = conformationPage.GetConfimationMessage();
